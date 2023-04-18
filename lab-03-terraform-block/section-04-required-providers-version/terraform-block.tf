45432f6d9c8f645a6da278fa4c227bdcc7bc703c
mgriffin78 <mgriffin78@hotmail.com>
2023-04-18 14:41:39 -0700

    Update terraform-block.tf

--- lab-03-terraform-block/section-04-required-providers-version/terraform-block.tf
+++ lab-03-terraform-block/section-04-required-providers-version/terraform-block.tf
@@ -1,6 +1,6 @@
 # Terraform Block
 terraform {
-  required_version = "= 1.0.0"
+  required_version = "~> 1.4.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,7 +8,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.5.1"
     }
   }
 }
